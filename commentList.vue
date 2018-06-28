a318e1a1ea541a73b485fbf9ae186b8522fad0f6
习惯五格 <33884004+xiguan-wuge@users.noreply.github.com>
2018-06-28 23:34:35 +0800

    vue 自定义组件实现评论列表和分页列表

---
+++ commentList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="comment-list">
+    <!-- <slot/> -->
+    <comment-item v-for="(item,index) in list" :content="item" :key="index"></comment-item>
+
+    <div v-if="list.length==0" class="emptyMsg">当前没有评论</div>
+  </div>
+</template>
+<script>
+import commentItem from './commentItem'
+export default {
+  props:{
+    'list':Array
+  },
+  components:{
+    commentItem
+  }
+  
+}
+
+</script>
+<style>
+
+</style>
