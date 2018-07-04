7ce43f09e9a9736ab3e9e3128ff2fc7509f2a398
习惯五格 <33884004+xiguan-wuge@users.noreply.github.com>
2018-07-04 23:40:43 +0800

    仿饿了吗

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,68 @@
 <template>
   <div id="app">
-    <router-link to='/'>/</router-link>
-    <router-link to='/about'>/about</router-link>
-    <router-link to='/dashboard'>/dashboard</router-link>
-    <router-link to='/login'>/login</router-link>
-    <router-link to='/logout'>/logout</router-link>
-    <router-view/>
+    <v-header :seller="seller"></v-header>
+    <div class="tab border-1px">
+      <div class="tab-item">
+        <router-link to="/goods">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评论</router-link>
+      </div>      
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>      
+    </div>
+    <keep-alive>
+      <router-view :seller="seller"></router-view>
+    </keep-alive>
   </div>
 </template>
 
 <script>
+import header from '@/components/header/header.vue';
+const response=require('@/common/data/seller.json')
+const ERR_OK=0;
 export default {
-  name: 'App'
+  name: 'App',
+  data(){
+    return{
+      seller:{
+        id:(()=>{
+          let queryParam=urlParse();
+          return queryParam.id;
+        })()
+      }
+    };
+  },
+  created(){
+    if(response.errno===ERR_OK){
+      this.seller=Object.assign({},this.seller,response.data)
+    }
+    
+  },
+  components: {
+    'v-header':header,
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" rel="stylesheet/stylus">
+@import "./common/stylus/mixin.styl"
+.tab
+  display:flex
+  width :100%;
+  height :40px
+  line-height :40px
+  //border-bottom:1px solid rgba(7,17,27,0.1)
+  border-1px(rgba(7,17,27,0.1))
+  .tab-item
+    flex:1
+    text-align:center
+    &>a
+      display :block
+      font-size 14px
+      color :rgb(77,85,93)
+      text-decoration none;
+      &.active
+        color:rgb(240,20,20)
 </style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,37 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import VueRouter from 'vue-router';
+import goods from '@/components/goods/goods';
+import ratings from '@/components/ratings/ratings';
+import seller from '@/components/seller/seller';
+import '@/common/stylus/index.styl'
+Vue.config.productionTip = false;
+Vue.use(VueRouter);
+const routes = [
+  {
+    path: "/",
+    component: goods
+  },
+  {
+    path: "/goods",
+    component: goods
+  },
+  {
+    path: "/ratings",
+    component: ratings
+  },
+  {
+    path: "/seller",
+    component: seller
+  }
+];
+const router =new VueRouter({
+  linkActiveClass:'active',
+  routes
+})
 
-Vue.config.productionTip = false
+
 
 /* eslint-disable no-new */
 new Vue({
