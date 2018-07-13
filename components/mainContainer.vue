63eccba534dd8b7c28d79800f72347b371ad790d
习惯五格 <33884004+xiguan-wuge@users.noreply.github.com>
2018-07-13 21:59:50 +0800

    eventBus + watch
    eventBus 解决的是兄弟组件之间的事件通信问题
    
    watch 监听变化

---
+++ components/mainContainer.vue
@@ -0,0 +1,25 @@
+<template> 
+  <div>
+    <click-component v-for="i in s" :key="i" :index="i">
+    </click-component>    
+    <show-component></show-component>
+  </div>
+</template>
+<script>
+import clickComponent from './click.vue'
+import showComponent from './show.vue'
+export default {
+  data(){
+    return{
+      s:5
+    }
+  },
+  components: {
+    clickComponent,
+    showComponent
+  }
+}
+</script>
+<style>
+</style>
+eventBus 解决的是兄弟组件之间的事件传递问题，本质是订阅发布者模式，摆托了兄弟组件需要父组件转达的问题，当然还有另一种解决办法--vuex,数据流，但是一般大项目引用vuex
--- main.js
+++ main.js
@@ -2,14 +2,17 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import './mock'
-Vue.use(ElementUI);
+import router from './router'
+import './config/rem'
+import './style/common.styl'
+import EventProxy from 'vue-event-proxy'
+Vue.use(EventProxy)
 Vue.config.productionTip = false
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router,
   components: { App },
   template: '<App/>'
 })
